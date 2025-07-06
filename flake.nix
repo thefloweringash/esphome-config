53d6211f1d48c98e1e5c29268aa92f0b1d75d6dd
Andrew Childs <lorne@cons.org.nz>
2025-07-06 14:53:07 +0900

    flake.nix: add support for aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -5,16 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
-      pkgs.mkShell {
-        buildInputs = [
-          pkgs.esphome
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      devShells = forEachSystem (system: {
+        default =
+          let pkgs = nixpkgs.legacyPackages.${system}; in
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.esphome
 
-          # keep this line if you use bash
-          pkgs.bashInteractive
-        ];
-      };
-  };
+              # keep this line if you use bash
+              pkgs.bashInteractive
+            ];
+          };
+        });
+    };
 }
